f567faa1d217429cde71356996567044ca16d280
sagadav <artursagadeev97@gmail.com>
2023-02-08 19:45:08 +0500

    load_dotenv in settings.py

--- lyceumsite/lyceumsite/settings.py
+++ lyceumsite/lyceumsite/settings.py
@@ -13,10 +13,13 @@ import json
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv(BASE_DIR.parent / ".env")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
