773b55f64ac3b9d2ace8f3d0f24bc0aff3ec6f69
sagadav <artursagadeev97@gmail.com>
2023-02-24 20:53:17 +0300

    transliterate

--- lyceumsite/core/utils.py
+++ lyceumsite/core/utils.py
@@ -1,18 +1,9 @@
 import re
 
+from transliterate import translit
+
 
 def normalize_name(name):
-    cyr_to_eng = str.maketrans(
-        {
-            "а": "a",
-            "е": "e",
-            "у": "y",
-            "х": "x",
-            "р": "p",
-            "с": "c",
-            "о": "o",
-            "к": "k",
-            "г": "r",
-        }
+    return re.sub(
+        r"[^a-zа-яеё]", "", translit(name.lower(), "ru", reversed=True)
     )
-    return re.sub(r"[^a-zа-яеё]", "", name.lower().translate(cyr_to_eng))
