213ce5ee0ffaafeed70c1f2cac5477e24879f675
sagadav <artursagadeev97@gmail.com>
2023-03-23 23:07:42 +0500

    linted

--- lyceumsite/users/tests.py
+++ lyceumsite/users/tests.py
@@ -1,7 +1,7 @@
+from django.contrib.auth.models import User
 from django.core import mail
 from django.test import Client, TestCase
 from django.urls import reverse
-from django.contrib.auth.models import User
 
 
 class UsersTests(TestCase):
