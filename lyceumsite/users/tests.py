436d9bc625f219b5a43f0224458298cf77ab3dd1
sagadav <artursagadeev97@gmail.com>
2023-03-23 23:19:03 +0500

    update tests

--- lyceumsite/users/tests.py
+++ lyceumsite/users/tests.py
@@ -16,6 +16,22 @@ class UsersTests(TestCase):
         self.assertIn("form", response.context)
         self.assertEqual(len(mail.outbox), 1)
 
+    def test_signup_activate_expire(self):
+        form_data = {
+            "email": "test@mail.ru",
+            "username": "test-uniq-test",
+            "password1": "123a",
+            "password2": "123a",
+        }
+        Client().post(reverse("users:signup"), data=form_data)
+        uid, token, _ = (
+            str(mail.outbox[0].message()).split("activate/")[1].split("/")
+        )
+        response_activate = Client().get(
+            reverse("users:activate", args=[uid, token])
+        )
+        self.assertTrue(response_activate.context["done"])
+
     def test_login(self):
         user = User.objects.create(
             email="test@hello.com",
