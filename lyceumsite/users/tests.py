6ec6c738ee9d37b6ce37a1f65f1b03c2622c4cee
sagadav <artursagadeev97@gmail.com>
2023-03-24 20:32:52 +0500

    tests

--- lyceumsite/users/tests.py
+++ lyceumsite/users/tests.py
@@ -1,6 +1,9 @@
+from datetime import datetime, timedelta
+
 from django.core import mail
 from django.test import Client, TestCase
 from django.urls import reverse
+from freezegun import freeze_time
 
 
 class UsersTests(TestCase):
@@ -15,7 +18,7 @@ class UsersTests(TestCase):
         self.assertIn("form", response.context)
         self.assertEqual(len(mail.outbox), 1)
 
-    def test_signup_activate_expire(self):
+    def test_signup_activate(self):
         form_data = {
             "email": "test@mail.ru",
             "username": "test-uniq-test",
@@ -30,3 +33,21 @@ class UsersTests(TestCase):
             reverse("users:activate", args=[uid, token])
         )
         self.assertTrue(response_activate.context["done"])
+
+    @freeze_time("2012-01-14")
+    def test_signup_activate_expire(self):
+        form_data = {
+            "email": "test@mail.ru",
+            "username": "test-uniq-test",
+            "password1": "123a",
+            "password2": "123a",
+        }
+        Client().post(reverse("users:signup"), data=form_data)
+        uid, token, _ = (
+            str(mail.outbox[0].message()).split("activate/")[1].split("/")
+        )
+        with freeze_time(datetime.now() + timedelta(hours=12, seconds=1)):
+            response_activate = Client().get(
+                reverse("users:activate", args=[uid, token])
+            )
+            self.assertFalse(response_activate.context["done"])
